f8833e1f49c20b7e6178c8e5359fca7fda648735
ddatsko <datsko.d@ucu.edu.ua>
2019-03-10 18:19:26 +0200

    added documentation, fixed bugs

--- battle.py
+++ battle.py
@@ -2,74 +2,143 @@ import os
 
 
 class Game:
+    """
+    Class fpr representing the whole game
+    """
+
     def __init__(self, fields, players, current_player):
+        """
+        (Game, list(Field), list(Player), Player) -> None
+        Initialize a class object
+        :param fields: list of 2 objects of class Field
+        :param players: list of 2 objects of class Player
+        :param current_player: Player object that is moving nof
+        """
         self.__fields = fields
-        self.__players = players
-        self.__current_player = current_player
+        self.players = players
+        self.current_player = current_player
 
     def shoot_at(self, index):
-        position = self.__players[index].read_position()
+        """
+        (Game, int) -> str
+        Shoot at the position
+        :param index: index of shooting player
+        :return: "win" if the player won after this shot,
+        "shot" if he shot the opponent's ship,
+        "missed" if player missed,
+        "" if player shot at the wrong position
+        """
+        position = self.players[index].read_position()
         res = self.__fields[index ^ 1].shoot_at(position)
         if not self.__fields[index ^ 1].are_ships():
             return "win"
         return res
 
     def field_with_ships(self, index):
+        """
+        (Game, int) -> str
+        :param index: index of the field to be shown
+        :return: str represantation of the field with all ships shown
+        """
         return self.__fields[index].field_view(True)
 
     def field_without_ships(self, index):
+        """
+        (Game, int) -> str
+        :param index: index of the field to be shown
+        :return: str representation of the field with only
+        shot ships shown
+        """
         return self.__fields[index].field_view(False)
+
+    def change_player(self):
+        """
+        (Game) -> None
+        Change current player for aother one
+        """
+        self.current_player = self.players[self.players.index(self.current_player) ^ 1]
 
 
 class Player:
     def __init__(self, name):
-        self.__name = name
+        """
+        (Player, str) -> None
+        Method for initializing object of class Player
+        :param name: name of the player
+        """
+        self.name = name
 
     def read_position(self):
+        """
+        (Player) -> (int, int)
+        Read position from user and convert it to right format
+        :return: position in format (int, int)
+        """
         pos = ""
         while not (len(pos) == 2 and 'a' <= pos[0] <= 'j' and '0' <= pos[1] <= '9'):
-            pos = input("{}, please enter the position: ".format(self.__name.lower()))
+            pos = input("{}, please enter the position: ".format(self.name.capitalize()))
         return (int(pos[1]), ord(pos[0]) - ord('a'))
 
 
 class Field:
     def __init__(self, ships=None):
+        """
+        (Field, list)
+        Initialize an object of class Field
+        :param ships: list of ships on the field
+        """
         self.__ships = ships
         self.__cells = [[None] * 10 for i in range(10)]
         self.shot = set()
 
     def get_ships_from_user(self, player):
+        """
+        (Field, Player) -> None
+        Get ships from player and add the on the field
+        :param player: Player object to take ships from
+        """
         ships = [0] * 4
+        # loop untill all the 10 ships are read
         while sum(ships) < 1:
             os.system("clear")
+
+            # show the field
             print("Current field: ")
             print(self.field_view(True))
             print()
+
+            # read position and ship details from the user
             x, y = player.read_position()
             orientation = input("Choose orientation: Vertical[v] of horizontal[h]: ")
             while not orientation or orientation not in "vh":
                 orientation = input("Try again [v/h]: ").lower()
             length = 0
-            try:
-                length = int(input("Select length [1..4]: "))
-                if orientation == "h":
-                    if y + length > 10:
-                        continue
-                else:
-                    if x + length > 10:
-                        continue
-                assert (ships[length - 1] <= 4 - length)
-            except:
-                print("Only numbers from 1 to 4 are acceptable")
-                while True:
-                    try:
-                        length = int(input("Try again [1..4]: "))
-                        assert (ships[length - 1] <= 4 - length)
-                        os.system("clear")
+
+            # read the length of the ship from user
+            while True:
+                try:
+                    length = int(input("Select length [1..4] or enter -1 to add another ship: "))
+                    # -1 means that user wants to choose another position or orientation
+                    if length == -1:
                         break
-                    except:
-                        continue
-            ships[length - 1] += 1
+
+                    # check the validity of chosen position
+                    if orientation == "h":
+                        if y + length > 10:
+                            continue
+                    else:
+                        if x + length > 10:
+                            continue
+                    assert (ships[length - 1] <= 4 - length)
+                    break
+                except:
+                    print("Only numbers from 1 to 4 are acceptable")
+
+            # if user wants to choose another position
+            if length == -1:
+                continue
+
+            # check if the chosen ship is near other ships
             y_mul = x_mul = 0
             if orientation == "h":
                 y_mul = 1
@@ -77,27 +146,40 @@ class Field:
                 x_mul = 1
             os.system("clear")
             for i in range(length):
-                if self.near_ship(x + i*x_mul, y + i*y_mul):
+                if self.near_ship(x + i * x_mul, y + i * y_mul):
                     print("This ship can not be so close to another one")
                     break
             else:
+                # add ship to the list of added ships and to the field
+                ships[length - 1] += 1
                 ship = Ship((x, y), orientation == "h", length)
                 for i in range(length):
-                    self.__cells[x + i * x_mul][y + i*y_mul] = ship
-
-
+                    self.__cells[x + i * x_mul][y + i * y_mul] = ship
 
     def near_ship(self, x, y):
+        """
+        (Field, int, int) -> bool
+        :param x: x-coordinate of the checked cell
+        :param y: y-coordinate of the checked cell
+        :return: if the cell is near the ship
+        """
         x_check = [0, -1, -1, 0, 1, 1, 1, 0, -1]
         y_check = [0, 0, 1, 1, 1, 0, -1, -1, -1]
         for i in range(9):
             if 0 <= x + x_check[i] < 10 and 0 <= y + y_check[i] < 10:
-                if self.__cells[x+x_check[i]][y + y_check[i]]:
+                if self.__cells[x + x_check[i]][y + y_check[i]]:
                     return True
         return False
 
-
     def shoot_at(self, position):
+        """
+        (Field, tuple(int, int)) -> str
+        :param position: position to shoot at
+        :return: "win" if the player won after this shot,
+        "shot" if he shot the opponent's ship,
+        "missed" if player missed,
+        "" if player shot at the wrong position
+        """
         x, y = position
         if (x, y) not in self.shot:
             if self.__cells[x][y]:
@@ -105,8 +187,13 @@ class Field:
             else:
                 self.shot.add((x, y))
                 return "missed"
+        return None
 
     def are_ships(self):
+        """
+        (Field) -> bool
+        :return: whether there still are ships on the field
+        """
         res = False
         for i in range(10):
             for j in range(10):
@@ -116,6 +203,11 @@ class Field:
         return res
 
     def field_view(self, show_ships):
+        """
+        (Field, bool) -> str
+        :param show_ships: whether not beaten ships should be shown
+        :return: string representation of the field
+        """
         res = ""
         res += '    '
         for i in range(10):
@@ -128,13 +220,19 @@ class Field:
                     cell = (self.__cells[i][j].view(i, j, show_ships))
                 else:
                     cell = "·" if (i, j) in self.shot else "_"
-                res += "|{}".format(cell)
-            res += "|\n"
+                res += "𝖨{}".format(cell)
+            res += "𝖨\n"
         return res
 
 
 class Ship:
     def __init__(self, bow, horizontal, length):
+        """
+        (Ship, tuple(int, int), bool, int)
+        :param bow: position of left top corner of the ship
+        :param horizontal: whether the ship is horizontal
+        :param length: length of the ship
+        """
         self.bow = bow
         self.horizontal = horizontal
         self.__length = length
@@ -148,15 +246,27 @@ class Ship:
                 self.fields.append((bow[0] + i, bow[1], False))
 
     def shoot_at(self, position):
+        """
+        (Ship, tuple(int, int)) -> str
+        :param position: position of the cell to be shoot
+        :return: "shot" if player shot at the ship else ""
+        """
         x, y = position
         if (x, y, True) in self.fields:
-            return False
+            return ""
         self.hit += 1
         self.fields.remove((x, y, False))
         self.fields.append((x, y, True))
         return "shot"
 
     def view(self, x, y, show_ships):
+        """
+        (Ship, int, int, bool) -> str
+        :param x: x-coordinate of the cell
+        :param y: y-coordinate of the cell
+        :param show_ships: whether undeaten part should be shown
+        :return: string representation of the cell
+        """
         if self.hit == self.__length:
             return "■"
         elif (x, y, True) in self.fields:
--- game.py
+++ game.py
@@ -1,36 +1,49 @@
 from battle import Field, Game, Player, Ship
 import os
 
+
 def main():
     os.system("clear")
+
+    # Get players names
     name2 = name1 = input("Enter the name of the first player: ")
     while name2 == name1:
         name2 = input("Enter the name of the second player: ")
+
+    # initialize game objects and the game itself
     player1, player2 = Player(name1), Player(name2)
     field1, field2 = Field(), Field()
     field1.get_ships_from_user(player1)
     field2.get_ships_from_user(player2)
-    game = Game([field1, field2], [player1, player2], 0)
-    counter = 0
+    game = Game([field1, field2], [player1, player2], player1)
+
+    # main loop of the game
     while True:
+        # extract current player and his index
+        player = game.current_player
+        player_index = game.players.index(player)
         res = False
-        os.system("clear")
-        print("Field of player {}".format(eval("name{}".format((counter ^ 1) + 1))))
-        print(game.field_without_ships(counter ^ 1))
+
+        # make player make a right move (loop if move is ot right or if he shot a ship)
         while not res or res == "shot":
-            res = game.shoot_at(counter)
-            if res == "shot":
-                os.system("clear")
-                print("Field of player {}".format(eval("name{}".format((counter ^ 1) + 1))))
-                print(game.field_without_ships(counter ^ 1))
+            # print opponent`s field
+            os.system("clear")
+            print("Field of player {}".format(game.players[player_index ^ 1].name))
+            print(game.field_without_ships(player_index ^ 1))
+
+            # make a shot and save it`s result to res
+            res = game.shoot_at(player_index)
+
+        # when someone wins
         if res == "win":
-            print("Player {} WON!!!".format(eval("name" + str(counter + 1))))
+            print("Player {} WON!!!".format(player.name))
             print("Field of player {}".format(name1))
             print(game.field_with_ships(0))
             print("Field of player {}".format(name2))
             print(game.field_with_ships(1))
             break
-        counter = counter ^ 1
+        # change current player
+        game.change_player()
 
 
 if __name__ == "__main__":
